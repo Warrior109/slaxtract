c0e154885c1ab4a6f92d990f406ba9e8310df33a
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-07-18 23:07:14 -0500

    Rescue file access errors

--- slaxtract.rb
+++ slaxtract.rb
@@ -19,15 +19,20 @@ end
 
 users = Slack.users_list
 
-CSV.open('dump.csv', 'wb') do |csv|
-  csv << ['Username', 'First Name', 'Last Name', 'Email']
-  users['members'].each do |user|
-    username = user['name']
-    first_name = user['profile']['first_name']
-    last_name = user['profile']['last_name']
-    email = user['profile']['email']
-    csv << [username, first_name, last_name, email]
+begin
+  CSV.open('dump.csv', 'wb') do |csv|
+    csv << ['Username', 'First Name', 'Last Name', 'Email']
+    users['members'].each do |user|
+      username = user['name']
+      first_name = user['profile']['first_name']
+      last_name = user['profile']['last_name']
+      email = user['profile']['email']
+      csv << [username, first_name, last_name, email]
+    end
   end
+rescue => e
+  puts "CSV Error: #{e}"
+  abort
 end
 
 puts "Exported #{users['members'].count} users to dump.csv."
